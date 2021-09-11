277b90f5df8f3e8b2360a3077c555012033b4930
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-11 09:49:31 +0000

    Resource datatype added

---
+++ src/Core/Data/Resource.ts
@@ -0,0 +1,30 @@
+class Resource {
+  private _name: string;
+  private _url: string;
+
+  constructor() {
+
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get url(): string {
+    return this._url;
+  }
+
+  set name(val: string) {
+    this._name = val;
+  }
+
+  set url(val: string) {
+    this._url = val;
+  }
+
+  public createNew(): Resource {
+    return new Resource();
+  }
+}
+
+export default Resource;
