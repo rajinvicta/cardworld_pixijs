36d223ef3d8a2b4ea5fa70ded2cde08be0712936
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-11 12:52:05 +0000

    Add loader to Game

--- src/Core/Game.ts
+++ src/Core/Game.ts
@@ -1,13 +1,15 @@
 import Config from './Control/Config';
-
 import IScreen from './Plugin/IScreen';
+import IGfxLoader from './Plugin/IGfxLoader';
 
 class Game {
   private _screen: IScreen;
   private _config: Config;
+  private _gfxLoader: IGfxLoader;
 
-  constructor(screen: IScreen, config: Config) {
+  constructor(screen: IScreen, gfxLoader: IGfxLoader, config: Config) {
     this._screen = screen;
+    this._gfxLoader = gfxLoader;
     this._config = config;
   }
 
