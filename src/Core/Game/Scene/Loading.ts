0884c4d1f60b7d1af51580ddc788b1af491899b4
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-12 18:52:59 +0000

    Created a basic scene

---
+++ src/Core/Game/Scene/Loading.ts
@@ -0,0 +1,23 @@
+import IScene from "../../Kernel/GameObjects/IScene";
+
+
+class Loading implements IScene {
+  constructor() {
+
+  }
+
+  public preload() {
+
+  }
+
+  public create() {
+    console.log("Hello World!");
+  }
+
+  public update() {
+
+  }
+
+}
+
+export default Loading;
