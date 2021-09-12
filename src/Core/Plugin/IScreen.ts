8af9aa13fb94f5ffd83355619f498e2e5f183386
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-12 07:11:40 +0000

    adding declarations for container creation

--- src/Core/Plugin/IScreen.ts
+++ src/Core/Plugin/IScreen.ts
@@ -1,5 +1,13 @@
 interface IScreen {
   startRenderer(width: number, height: number, antialias: boolean, transparent: boolean): void;
+
+  createContainer(
+    particleMode: boolean,
+    maxSize: number,
+    properties: any,
+    batchSize?: number,
+    autoResize?: boolean
+  ): any;
 }
 
 export default IScreen;
