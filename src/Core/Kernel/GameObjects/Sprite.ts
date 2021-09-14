05b6aa417f2c4a610737ea1683e47815c5d4de47
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-14 03:58:08 +0000

    Sprite creates and add PIXI.Sprites

--- src/Core/Kernel/GameObjects/Sprite.ts
+++ src/Core/Kernel/GameObjects/Sprite.ts
@@ -3,14 +3,25 @@ import CoreEntity from './CoreEntity';
 import Position from './Component/Position';
 import Display from './Component/Display';
 import IGameObject from '../../Plugin/IGameObject';
+import IScreen from "../../Plugin/IScreen";
+import ISceneManager from "../../Plugin/ISceneManager";
 
 class Sprite extends CoreEntity {
-  constructor(position: Position, display: Display, foreignObject: IGameObject) {
+  private _screen: IScreen;
+  private _sceneManager: ISceneManager;
+
+  constructor(position: Position, display: Display, foreignObject: IGameObject, screen: IScreen, sceneManager: ISceneManager) {
     super(position, display, foreignObject);
+
+    this._screen = screen;
+    this._sceneManager = sceneManager;
   }
 
-  public init(x: number, y: number, sheet: string, frame: string) {
-    
+  public init(x: number, y: number, sheet: string, frame?: string) {
+    let fo = this._screen.createSprite(sheet, frame);
+
+    this._sceneManager.addObject(fo);
+    this._activate(x, y, fo);
   }
 }
 
