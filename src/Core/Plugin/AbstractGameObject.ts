6a39e78d8949bb14dd498d6443109dc496f9035d
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-16 03:43:56 +0000

    Deny access to AbstractGameObject when not properly subsituted

--- src/Core/Plugin/AbstractGameObject.ts
+++ src/Core/Plugin/AbstractGameObject.ts
@@ -2,28 +2,90 @@ import IAbstractGameObject from "./IAbstractGameObject";
 //Place holder object allocated by default to avoid IAbstractGameObject | null situations
 
 class AbstractGameObject implements IAbstractGameObject {
-  public x: number;
-  public y: number;
-  public angle: number;
-  public visible: boolean;
-  public alpha: number;
-  public style: any;
-  public text: string;
-  public data: any;
+  get x(): number {
+    this._denyAccess();
+    return 0;
+  }
+
+  get y(): number {
+    this._denyAccess();
+    return 0;
+  }
+
+  get angle(): number {
+    this._denyAccess();
+    return 0;
+  }
+
+  get visible(): boolean {
+    this._denyAccess();
+    return true;
+  }
+
+  get alpha(): number {
+    this._denyAccess();
+    return 1;
+  }
+
+  get style(): any {
+    this._denyAccess();
+    return {};
+  }
+
+  get text(): string {
+    this._denyAccess();
+    return "";
+  }
+
+  get data(): any {
+    this._denyAccess();
+    return {};
+  }
+
+
+  set x(val: number) {
+    this._denyAccess();
+  }
+
+  set y(val: number) {
+    this._denyAccess();
+  }
+
+  set angle(val: number) {
+    this._denyAccess();
+  }
+
+  set visible(val: boolean) {
+    this._denyAccess();
+  }
+
+  set alpha(val: number) {
+    this._denyAccess();
+  }
+
+  set style(val: any) {
+    this._denyAccess();
+  }
+
+  set text(val: string) {
+    this._denyAccess();
+  }
+
+  set data(val: any) {
+    this._denyAccess();
+  }
+
 
   constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.angle = 0;
-    this.visible = true;
-    this.alpha = 1;
-    this.text = "";
-    this.style = {};
-    this.data = {};
+
   }
 
   public createNew(): IAbstractGameObject {
     return new AbstractGameObject();
+  }
+
+  private _denyAccess() {
+    console.error("Can not access the AbstractGameObject!");
   }
 }
 
