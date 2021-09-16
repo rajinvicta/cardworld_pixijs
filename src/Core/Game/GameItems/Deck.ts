002471f5902c1b6345c5796c6ae6188226e8a5e1
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-16 08:22:59 +0000

    Adding decks

---
+++ src/Core/Game/GameItems/Deck.ts
@@ -0,0 +1,57 @@
+import EntityFactory from "../../Kernel/GameObjects/EntityFactory";
+import Sprite from "../../Kernel/GameObjects/Sprite";
+
+class Deck {
+  private _entityFactory: EntityFactory;
+  private _cards: Sprite[];
+
+  private _maxCards: number;
+  private _cardHeight: number;
+  private _cardStart: number;
+  private _cardGap: number;
+
+  constructor(entityFactory: EntityFactory) {
+    this._entityFactory = entityFactory;
+
+    this._cards = [];
+    this._maxCards = 72;
+    this._cardHeight = 232;
+    this._cardStart = 250;
+    this._cardGap = 7;
+  }
+
+  public init(x: number) {
+    this._populateCards(x);
+  }
+
+  public moveBack(card: Sprite) {
+
+  }
+
+  private _populateCards(x: number) {
+    for (let c = 0; c < this._maxCards; c++) {
+      let cardName = "card_" + this._rnd(1, 4);
+
+      let card = this._entityFactory.sprite(x, this._cardStart + (c * this._cardGap), 'main', cardName);
+
+      this._cards.push(card);
+    }
+  }
+
+  private _getNextXY(): {x: number, y: number} {
+    let x = this._cards[0].position.x;
+    let y = this._cardStart + (this._cards.length * this._cardGap);
+
+    return {x: x, y: y};
+  }
+
+  private _rnd(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+}
+
+export default Deck;
--- src/Core/Game/Scene/CardMode.ts
+++ src/Core/Game/Scene/CardMode.ts
@@ -2,9 +2,11 @@ import EntityFactory from "../../Kernel/GameObjects/EntityFactory";
 
 import Sprite from "../../Kernel/GameObjects/Sprite";
 import Button from "../GameItems/Button";
+import Deck from "../GameItems/Deck";
 
 import IScene from "../../Kernel/GameObjects/IScene";
 import ISceneManager from "../../Plugin/ISceneManager";
+
 
 
 class CardMode implements IScene {
@@ -12,10 +14,15 @@ class CardMode implements IScene {
   private _sceneManager: ISceneManager;
   private _back: Button;
   private _background: Sprite;
+  private _deck1: Deck;
+  private _deck2: Deck;
 
-  constructor(entityFactory: EntityFactory, sceneManager: ISceneManager, button: Button, sprite: Sprite) {
+  constructor(entityFactory: EntityFactory, sceneManager: ISceneManager, button: Button, sprite: Sprite,
+  deck1: Deck, deck2: Deck) {
     this._entityFactory = entityFactory;
     this._sceneManager = sceneManager;
+    this._deck1 = deck1;
+    this._deck2 = deck2;
 
     this._back = button;
 
@@ -30,6 +37,7 @@ class CardMode implements IScene {
     console.log("CardMode Level");
     this._initBackground();
     this._initButtons();
+    this._initCards();
   }
 
   public update() {
@@ -48,6 +56,11 @@ class CardMode implements IScene {
     this._back.init(25, 50, 'back_btn', () => {
       this._sceneManager.startScene('Menu');
     });
+  }
+
+  private _initCards() {
+    this._deck2.init(814);
+    this._deck1.init(100);
   }
 
 }
