d94981f1ffab7552ac40489355020fd079dd96ac
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2021-09-16 06:11:18 +0000

    Buttons added

---
+++ src/Core/Game/GameItems/Button.ts
@@ -0,0 +1,26 @@
+import Sprite from "../../Kernel/GameObjects/Sprite";
+import EntityFactory from "../../Kernel/GameObjects/EntityFactory";
+
+class Button {
+  private _entityFactory: EntityFactory;
+  private _sprite: Sprite;
+
+  constructor(entityFactory: EntityFactory, sprite: Sprite) {
+    this._entityFactory = entityFactory;
+    this._sprite = sprite;
+  }
+
+  get sprite(): Sprite {
+    return this._sprite;
+  }
+
+  public init(x: number, y: number, frame: string) {
+    this._sprite = this._entityFactory.sprite(x, y, 'main', frame);
+  }
+
+  public createNew(): Button {
+    return new Button(this._entityFactory, this._sprite.createNew());
+  }
+}
+
+export default Button;
--- src/Core/Game/Scene/Menu.ts
+++ src/Core/Game/Scene/Menu.ts
@@ -1,13 +1,21 @@
 import EntityFactory from "../../Kernel/GameObjects/EntityFactory";
 
+import Button from "../GameItems/Button";
+
 import IScene from "../../Kernel/GameObjects/IScene";
 
 
 class Menu implements IScene {
   private _entityFactory: EntityFactory;
+  private _card: Button;
+  private _mix: Button;
+  private _fire: Button;
 
-  constructor(entityFactory: EntityFactory) {
+  constructor(entityFactory: EntityFactory, button: Button) {
     this._entityFactory = entityFactory;
+    this._card = button;
+    this._mix = button;
+    this._fire = button;
   }
 
   public async preload(): Promise<void> {
@@ -16,6 +24,7 @@ class Menu implements IScene {
 
   public create() {
     console.log("Menu Level");
+    this._initButtons();
   }
 
   public update() {
@@ -24,6 +33,18 @@ class Menu implements IScene {
 
   public shutdown() {
 
+  }
+
+  private  _initButtons() {
+    this._card = this._card.createNew();
+    this._mix = this._mix.createNew();
+    this._fire = this._fire.createNew();
+
+    let startY = 900; 
+
+    this._card.init(303, startY + 0, 'btn_card');
+    this._mix.init(303, startY + 250, 'btn_mixed');
+    this._fire.init(303, startY + 500, 'btn_fire');
   }
 
 }
